8648b64a20ca920d2d5f7fa4cee209e6360debb1
Anzhela Savina <masterpeach@Anzhelas-MacBook-Air.local>
2025-03-25 17:23:37 -0400

    Проверка ответа Lumi

--- app.py
+++ app.py
@@ -1,14 +1,12 @@
 from flask import Flask, request
-from os import environ
 
 app = Flask(__name__)
 
-@app.route('/whatsapp', methods=['POST'])
+@app.route("/whatsapp", methods=["POST"])
 def whatsapp():
     incoming_msg = request.values.get('Body', '').lower()
-    return f'Lumi received: {incoming_msg}'
+    return f"Lumi received: {incoming_msg}"
 
-if __name__ == '__main__':
-    port = int(environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
- 
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
+
