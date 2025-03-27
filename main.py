a6678ea4593d7e14ddc9507ccec4cac9ab703db1
Anzhela Savina <masterpeach@Anzhelas-MacBook-Air.local>
2025-03-27 00:15:58 -0400

    Добавила twilio в requirements.txt

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, Response
+from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse
 
 app = Flask(__name__)
@@ -6,13 +6,10 @@ app = Flask(__name__)
 @app.route("/whatsapp", methods=["POST"])
 def whatsapp_reply():
     incoming_msg = request.values.get('Body', '').lower()
-    print("Получено сообщение:", incoming_msg)
-
     resp = MessagingResponse()
     msg = resp.message()
-    print("Отправляем ответ Lumi!")
     msg.body("Hello, this is Lumi speaking!")
+    return str(resp)
 
-    return Response(str(resp), mimetype="application/xml")
 
 
